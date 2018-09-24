15db09821819943510c18eb22f0c5d6adbfdb2bc
andi <amayer80@gmail.com>
2018-09-24 11:56:29 +0200

    Initial commit

---
+++ src/com/andi/Shape.java
@@ -0,0 +1,6 @@
+package com.andi;
+
+public interface Shape {
+
+    void draw();
+}
---
+++ src/com/andi/ShapeFactory.java
@@ -0,0 +1,19 @@
+package com.andi;
+
+import java.util.HashMap;
+
+public class ShapeFactory {
+
+    private static final HashMap circleMap = new HashMap();
+
+    public static Shape getCircle(String color) {
+        Circle circle = (Circle) circleMap.get(color);
+
+        if (circle == null) {
+            circle = new Circle(color);
+            circleMap .put(color, circle);
+            System.out.println("making circle of color: " + color);
+        }
+        return circle;
+    }
+}
